e34b8f5ada6fd76aa7ebd0c8e5e96c283c03cf46
AlexLucs <alexanderlucs@gmail.com>
2025-01-04 22:04:03 -0300

    Add files via upload

--- btc_real_time.py
+++ btc_real_time.py
@@ -1,58 +1,95 @@
 import os
 import webview
+from filelock import FileLock
+from screeninfo import get_monitors
+
 
 path = os.path.dirname(os.path.abspath(__file__)) + "/"
-controle_arquivo = path + "aberta.txt"
-
-
-def on_closed():
-    if os.path.exists(controle_arquivo):
-        os.remove(controle_arquivo)
-
+lockfile = path + "btc_real_time.lock"
 
 def reload():
     global window
     window.evaluate_js("location.reload()")
-
 
 def fechar():
     global window
     if window:
         window.destroy()
 
+def minimizar():
+    global window
+    if window:
+        window.minimize()
 
+def on_loaded():
+    global window
+    window.show()
+
+def obter_posicao_monitor_e_centralizar(window_width, window_height):
+    """
+    Função para obter as informações do monitor e calcular a posição centralizada
+    para a janela.
+    """
+    # Obter informações sobre os monitores
+    monitors = get_monitors()
+
+    if not monitors:
+        print("Nenhum monitor encontrado.")
+        return 0, 0  # Se não houver monitores, retorna posição 0,0
+
+    # Pegar o primeiro monitor (pode alterar para outro índice se necessário)
+    monitor = monitors[0]
+    monitor_width = monitor.width
+    monitor_height = monitor.height
+    monitor_x = monitor.x
+    monitor_y = monitor.y
+
+    # Calcular a posição da janela para centralizá-la
+    x_position = monitor_x + (monitor_width - window_width) // 2
+    y_position = monitor_y + (monitor_height - window_height) // 2
+
+    return x_position, y_position
 def abrir_janela():
     global window
-    if os.path.exists(controle_arquivo):
-        print("A janela já está aberta.")
-        return
+    lock = FileLock(lockfile)
 
-    with open(controle_arquivo, "w") as f:
-        f.write("1")
+    try:
+        with lock:
+            menu_items = [
+                webview.menu.MenuAction("Atualizar", reload),
+                webview.menu.MenuAction("Minimizar", minimizar),
+                webview.menu.MenuAction("Fechar", fechar),
+            ]
 
-    menu_items = [
-        webview.menu.MenuAction("Atualizar", reload),
-        webview.menu.MenuAction("Fechar", fechar),
-    ]
+            # url = "https://alexlucs.github.io/HTML/index.html"
+            url = os.environ.get("USERPROFILE")+"/Downloads/Bot/btcstatus/index.html"
 
-    url = "https://alexlucs.github.io/HTML/index.html"
+            window_width = 500
+            window_height = 950
 
-    window = webview.create_window(
-        "Stack Stats",
-        url,
-        width=500,
-        height=950,
-        frameless=True,
-        background_color="#000000",
-        on_top=True,
-        x=2077,
-        y=-1,
-    )
+            x_position, y_position = obter_posicao_monitor_e_centralizar(window_width, window_height)
 
-    window.events.closed += on_closed
+            window = webview.create_window(
+                "Stack Stats",
+                url,
+                width=window_width,
+                height=window_height,
+                frameless=True,
+                background_color="#000000",
+                on_top=True,
+                x=x_position,
+                y=y_position,
+            )
 
-    webview.start(menu=menu_items)
+            window.events.loaded += on_loaded
 
+            webview.start(menu=menu_items)
+
+    except Exception as e:
+        print(f"Erro ao tentar abrir a instância: {e}")
 
 if __name__ == "__main__":
-    abrir_janela()
+    if not os.path.exists(lockfile):
+        abrir_janela()
+    else:
+        print("Instância já está em execução.")

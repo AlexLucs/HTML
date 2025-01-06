680df87fb97762e2e40b64c1266dbc76ecc389eb
AlexLucs <alexanderlucs@gmail.com>
2025-01-06 05:10:21 -0300

    Add files via upload

--- btc_real_time.py
+++ btc_real_time.py
@@ -2,35 +2,71 @@ import os
 import sys
 import webview
 from screeninfo import get_monitors
+import pygetwindow as gw
 
-path = os.path.dirname(os.path.abspath(__file__)) + "/"
 
-class Api():
+class exposedApi():
     def fechar(var=None):
         global window
         if window:
             window.destroy()
 
-def obter_posicao_monitor_e_centralizar(window_width, window_height):
-    monitors = get_monitors()
+    def minimizar(var=None):
+        global window
+        if window:
+            window.minimize()
 
-    if not monitors:
-        return 0, 0
+    def centralizar(var=None, move=None):
+        global window
+        monitors = get_monitors()
 
-    if len(monitors) > 1:
-        monitor = monitors[1]
-    else:
-        monitor = monitors[0]
+        if not monitors:
+            return 0, 0
 
-    monitor_width = monitor.width
-    monitor_height = monitor.height
-    monitor_x = monitor.x
-    monitor_y = monitor.y
+        if len(monitors) > 1:
+            monitor = monitors[1]
+        else:
+            monitor = monitors[0]
+        
+        monitor_width = monitor.width
+        monitor_height = monitor.height
+        monitor_x = monitor.x
+        monitor_y = monitor.y
 
-    x_position = monitor_x + (monitor_width - window_width) // 2
-    y_position = monitor_y + (monitor_height - window_height) // 2
+        x_position = monitor_x + (monitor_width - 500) // 2
+        y_position = monitor_y + (monitor_height - 950) // 2
+       
+        if move==1:            
+            
+            window_title = 'Stack Stats'
+            windows = gw.getWindowsWithTitle(window_title)
 
-    return x_position, y_position
+            if windows:
+                window1 = windows[0]
+                x_position = window1.left
+                y_position = window1.top
+                
+                if x_position > -1:
+                    monitor = monitors[0]
+                else:
+                    monitor = monitors[1]
+                
+                monitor_width = monitor.width
+                monitor_height = monitor.height
+                monitor_x = monitor.x
+                monitor_y = monitor.y
+
+                x_position = monitor_x + (monitor_width - 500) // 2
+                y_position = monitor_y + (monitor_height - 950) // 2
+        
+            else:
+                x_position = 0
+                y_position = 0
+            
+            window.move(x_position, y_position)
+        
+        return x_position, y_position
+
 
 def abrir_janela():
     global window
@@ -39,27 +75,25 @@ def abrir_janela():
         # url = "https://alexlucs.github.io/HTML/index.html"
         url = os.environ.get("USERPROFILE") + "/Downloads/Bot/btcstatus/index.html"
 
-        window_width = 500
-        window_height = 950
-
-        x_position, y_position = obter_posicao_monitor_e_centralizar(window_width, window_height)
+        x_position, y_position = exposedApi.centralizar()
 
         window = webview.create_window(
             "Stack Stats",
             url,
-            width=window_width,
-            height=window_height,
+            width=500,
+            height=950,
             frameless=True,
             background_color="#000000",
             x=x_position,
             y=y_position,
-            js_api=Api()
+            js_api=exposedApi()
         )
 
         webview.start()
-        
+
     except Exception as e:
         print(f"Erro ao tentar abrir: {e}")
 
+    
 if __name__ == "__main__":
     abrir_janela()
